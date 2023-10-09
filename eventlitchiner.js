63ad46781092f6f66313750646b04de0d6af22ef
Rohul1 <rohul669319@gmail.com>
2023-10-09 12:53:34 +0600

    added all

--- eventlitchiner.js
+++ eventlitchiner.js
@@ -9,8 +9,15 @@ depositBtn.addEventListener("click", function () {
 
     let depositFieldString = depositField.value
     console.log(depositFieldString);
-
     depositField.value = "";
+
+    if (depositFieldString > 0) {
+        alert("Ok")
+    }
+    else {
+        alert("Please enter positive amount")
+    }
+
 
     let depositTotal = document.getElementById("Deposit-total");
     let PreviousDepositTotalString = depositTotal.innerText
@@ -27,5 +34,36 @@ depositBtn.addEventListener("click", function () {
     let newBalanceTotal = parseInt(previousBalanceTotalString) + parseInt(depositFieldString);
     balanceTotal.innerText = newBalanceTotal;
     console.log(newBalanceTotal);
+
+
+})
+let withdrawBtn = document.getElementById("btn-withdraw");
+withdrawBtn.addEventListener("click", function () {
+    console.log("withdraw btn clicked")
+
+    let withdrawField = document.getElementById("withdraw-field");
+
+    let withdrawFieldString = withdrawField.value
+    console.log(withdrawFieldString);
+
+    withdrawField.value = "";
+
+
+
+    let withdrawTotal = document.getElementById("Withdraw-total");
+    let previousWithdrawTotalString = withdrawTotal.innerText;
+    console.log(previousWithdrawTotalString);
+
+
+    let newWithdrawTotal = parseInt(previousWithdrawTotalString) + parseInt(withdrawFieldString);
+    withdrawTotal.innerText = newWithdrawTotal;
+    console.log(newWithdrawTotal);
+
+    let balanceTotal2 = document.getElementById("Balance-total");
+    let previousBalanceTotalString2 = balanceTotal2.innerText;
+
+    let newBalanceTotal2 = parseInt(previousBalanceTotalString2) - parseInt(withdrawFieldString);
+    balanceTotal2.innerText = newBalanceTotal2;
+    console.log(newBalanceTotal2)
 
 })
