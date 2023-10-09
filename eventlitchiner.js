f41f0249f4ff6f30a27b9a22c41914be699a6759
Rohul1 <rohul669319@gmail.com>
2023-10-09 11:26:06 +0600

    added balance

--- eventlitchiner.js
+++ eventlitchiner.js
@@ -10,12 +10,22 @@ depositBtn.addEventListener("click", function () {
     let depositFieldString = depositField.value
     console.log(depositFieldString);
 
+    depositField.value = "";
 
     let depositTotal = document.getElementById("Deposit-total");
     let PreviousDepositTotalString = depositTotal.innerText
     console.log(PreviousDepositTotalString);
 
 
-    let newDepositTotal = PreviousDepositTotalString + depositFieldString;
-    console.log(newDepositTotal);
+    let newDepositTotal = parseInt(PreviousDepositTotalString) + parseInt(depositFieldString);
+    depositTotal.innerText = newDepositTotal;
+
+    let balanceTotal = document.getElementById("Balance-total");
+    let previousBalanceTotalString = balanceTotal.innerText;
+
+
+    let newBalanceTotal = parseInt(previousBalanceTotalString) + parseInt(depositFieldString);
+    balanceTotal.innerText = newBalanceTotal;
+    console.log(newBalanceTotal);
+
 })
